caa0891c8e026b58be37e5f5ce98aeb484f3041c
Andrew Bell <fiixed@gmail.com>
2014-01-15 13:51:24 +1100

    initial commit

---
+++ NerdLauncher/src/main/java/com/fiixed/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,11 @@
+package com.fiixed.nerdlauncher;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by abell on 1/15/14.
+ */
+public class NerdLauncherFragment extends ListFragment {
+
+
+}
